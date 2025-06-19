4639abe6c08a5bd5eaf683efbba24ab961398bad
Wan2sleep <ratteeth5504@gmail.com>
2025-06-19 07:30:16 +0000

    main

--- Rush00/ex00/main.py
+++ Rush00/ex00/main.py
@@ -1,14 +1,24 @@
-'''Main file'''
+'''Main file - includes both Success and Fail cases'''
 from checkmate import checkmate
 
 def main():
-    board = """\
+    print("=== Case 1: King is in check (Expect: Success) ===")
+    board1 = """\
 ....
 .K..
 ....
 .Q..\
 """
-    checkmate(board)
+    checkmate(board1)
+
+    print("=== Case 2: King is safe (Expect: Fail) ===")
+    board2 = """\
+....
+.K..
+....
+...Q\
+"""
+    checkmate(board2)
 
 if __name__ == "__main__":
     main()
