ba93d30d7eed32213b80dc470a836932e98115d9
Wan2sleep <ratteeth5504@gmail.com>
2025-06-20 02:44:17 +0000

    eiei

--- cell05/ex05/aff_first_param.py
+++ cell05/ex05/aff_first_param.py
@@ -1,6 +1,15 @@
 import sys
 
-if len(sys.argv) > 1:
-    print(sys.argv[1])
-else:
-    print("none")
+def main():
+    if len(sys.argv) > 1:
+        print(sys.argv[1])
+    else:
+        user_input = input("Please enter a parameter: ").strip()
+        if user_input:
+            print(user_input)
+        else:
+            print("none.")
+        
+
+if __name__ == "__main__":
+    main()
